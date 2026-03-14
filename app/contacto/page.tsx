bd88357ad9cf88e93e30a16fb983110f7f6cf582
Rodrigo Jacome <rod.jacome12@gmail.com>
2026-03-14 00:51:01 -0600

    feat: test

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -3,8 +3,7 @@ export default function TrabajoPage() {
     <div className="p-10">
       <h1 className="text-3xl font-bold">Contactos</h1>
       <p className="mt-4 text-gray-600">
-        Aquí mostramos las acciones que realizamos para rescatar y cuidar
-        animales.
+        Aquí mostramos los contactos de las asociaciones
       </p>
     </div>
   );
