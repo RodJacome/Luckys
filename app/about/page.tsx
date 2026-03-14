2c862a6a981730e74287883a671e07ddeae726e7
Rodrigo Jacome <rod.jacome12@gmail.com>
2026-03-14 00:46:22 -0600

    feat: adding ref to Links

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">Acerca de nosotros</h1>
+      <p className="mt-4 text-gray-600">
+        Aquí mostramos las acciones que realizamos para rescatar y cuidar
+        animales.
+      </p>
+    </div>
+  );
+}
---
+++ app/contacto/page.tsx
@@ -0,0 +1,11 @@
+export default function TrabajoPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">Contactos</h1>
+      <p className="mt-4 text-gray-600">
+        Aquí mostramos las acciones que realizamos para rescatar y cuidar
+        animales.
+      </p>
+    </div>
+  );
+}
