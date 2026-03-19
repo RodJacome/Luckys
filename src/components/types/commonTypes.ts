2f41fcba8ebaea583b27238876321c1ab85f7b6e
Rodrigo Jacome <rod.jacome12@gmail.com>
2026-03-18 18:02:24 -0600

    Feat: adding filter to adoption page

--- src/components/types/commonTypes.ts
+++ src/components/types/commonTypes.ts
@@ -1,14 +1,20 @@
 export type AdoptionStatus = "adopcion" | "espera" | "adoptado";
+export type GenderType = "macho" | "hembra";
+export type AgeType = "0-6meses" | "6meses-1año" | "1año-5años" | "+5años";
+export type SizeType = "pequeño" | "mediano" | "grande";
 
 export type AdoptionType = {
   id: string;
   picture: string;
   name: string;
   raza: string;
-  tamaño: string;
+  tamaño: SizeType;
   asociacion?: string;
   status: AdoptionStatus;
   location: string;
+  genero: GenderType;
+  edad: AgeType;
+  especial?: string;
 };
 
 export type CarouselType = {
@@ -17,4 +23,17 @@ export type CarouselType = {
   title: string;
   description: string;
   url: string;
+};
+
+export type FilterCategory = "genero" | "edad" | "tamaño" | "location";
+
+export type FilterGroupType<T> = {
+  title: string;
+  options: T[];
+  category: FilterCategory;
+  handleCheckbox: (
+    category: FilterCategory,
+    value: T,
+    checked: boolean,
+  ) => void;
 };
